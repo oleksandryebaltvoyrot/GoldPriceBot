04587ebbfe38c7bc9faa4b1115e09f6bbb320652
olexandr-mosin <olexandr.mosin@ring.com>
2020-03-22 16:38:30 +0200

    add web run

--- src/main/java/utils/MarketplaysHelper.java
+++ src/main/java/utils/MarketplaysHelper.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 
 public class MarketplaysHelper {
     private static final Logger logger = LogManager.getLogger(MarketplaysHelper.class);
-    private static String url = "https://www.microsoft.com/en-us/p/xbox-live-gold/CFQ7TTC0K5DJ/000C";
+    private static String url = "https://www.microsoft.com/en-gb/p/xbox-live-gold/CFQ7TTC0K5DJ/000C";
     private static OkHttpClient client = new OkHttpClient();
     private static String run(String url) throws IOException {
         Request request = new Request.Builder()
@@ -30,7 +30,7 @@ public class MarketplaysHelper {
 
         Response response = client.newCall(request).execute();
         String responseBody = response.body().string();
-        logger.info(responseBody.trim().substring(0,100));
+        logger.info(responseBody);
         return responseBody;
     }
 
