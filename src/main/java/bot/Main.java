7473326ea82a7de919e290b7f88782eb860b1260
olexandr-mosin <olexandr.mosin@ring.com>
2020-03-22 15:43:14 +0200

    add web run

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -8,7 +8,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-
 public class Main {
     public static void main(String[] args) {
 
