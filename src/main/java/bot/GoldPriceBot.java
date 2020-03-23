0cb912969723eade0018d6ad50f10aee194c3613
olexandr-mosin <olexandr.mosin@ring.com>
2020-03-23 15:51:29 +0200

    change client

--- src/main/java/bot/GoldPriceBot.java
+++ src/main/java/bot/GoldPriceBot.java
@@ -5,13 +5,12 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import utils.MarketplaysHelper;
 import utils.XboxNowHelper;
 
 import java.util.List;
 
 import static services.StorageService.*;
-import static utils.MarketplaysHelper.collectInfo;
+import static utils.XboxNowHelper.collectInfo;
 
 public class GoldPriceBot extends TelegramLongPollingBot {
     @Override
