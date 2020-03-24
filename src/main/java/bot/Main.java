652f45d590c81d8b5659d7eca901e6a14cb8f74e
olexandr-mosin <olexandr.mosin@ring.com>
2020-03-24 17:15:57 +0200

    add spring

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -2,31 +2,34 @@ package bot;
 
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) throws TelegramApiRequestException {
+    public static void main(String[] args) {
 
         ApiContextInitializer.init();
 
         TelegramBotsApi botsApi = new TelegramBotsApi();
-        botsApi.registerBot(new GoldPriceBot());
 
-//        try {
-//            botsApi.registerBot(new GoldPriceBot());
-//            ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-//            Runnable task = () -> {
-//                try {
-//                    new GoldPriceBot().sendPriceChangedMessage("test");
-//                    new GoldPriceBot().dailyPriceCheck();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            };
-//
-//            executor.scheduleWithFixedDelay(task, 0, 2, TimeUnit.MINUTES);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        try {
+            botsApi.registerBot(new GoldPriceBot());
+            ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+            Runnable task = () -> {
+                try {
+                    new GoldPriceBot().sendPriceChangedMessage("test");
+                    new GoldPriceBot().dailyPriceCheck();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            };
+
+            executor.scheduleWithFixedDelay(task, 0, 2, TimeUnit.MINUTES);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
