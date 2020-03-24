560e580b4cd1eefb30bcdac48309ab867b9347e7
olexandr-mosin <olexandr.mosin@ring.com>
2020-03-24 19:07:49 +0200

    add spring

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -1,15 +1,18 @@
 package bot;
 
+import com.sun.net.httpserver.HttpServer;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //heroku ps:scale worker=1 -a xboxsubscriptionchecker
 
@@ -33,5 +36,18 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        int serverPort = Integer.parseInt(System.getenv("PORT"));
+        HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
+        server.createContext("/api/hello", (exchange -> {
+            String respText = "Hello!";
+            exchange.sendResponseHeaders(200, respText.getBytes().length);
+            OutputStream output = exchange.getResponseBody();
+            output.write(respText.getBytes());
+            output.flush();
+            exchange.close();
+        }));
+        server.setExecutor(null); // creates a default executor
+        server.start();
     }
 }
