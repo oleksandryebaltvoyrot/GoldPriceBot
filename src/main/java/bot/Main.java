c19c2578092c691ff5b7af8234aeeb5e08014901
olexandr-mosin <olexandr.mosin@ring.com>
2020-03-24 22:36:14 +0200

    add spring

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -3,38 +3,20 @@ package bot;
 import com.sun.net.httpserver.HttpServer;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, TelegramApiRequestException {
 
         //heroku ps:scale worker=1 -a xboxsubscriptionchecker
 
         ApiContextInitializer.init();
-
         TelegramBotsApi botsApi = new TelegramBotsApi();
-
-        try {
-            botsApi.registerBot(new GoldPriceBot());
-            ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
-            Runnable task = () -> {
-                try {
-                    new GoldPriceBot().sendPriceChangedMessage("test" + Thread.currentThread().getName());
-                    new GoldPriceBot().dailyPriceCheck();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            };
-
-            //executor.scheduleWithFixedDelay(task, 1, 2, TimeUnit.MINUTES);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        botsApi.registerBot(new GoldPriceBot());
 
         int serverPort = Integer.parseInt(System.getenv("PORT"));
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
@@ -44,7 +26,7 @@ public class Main {
             OutputStream output = exchange.getResponseBody();
             output.write(respText.getBytes());
             output.flush();
-            new GoldPriceBot().sendPriceChangedMessage("test" + Thread.currentThread().getName());
+            new GoldPriceBot().dailyPriceCheck();
             exchange.close();
         }));
         server.setExecutor(null); // creates a default executor
