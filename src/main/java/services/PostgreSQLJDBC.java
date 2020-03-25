fece8631aac32a77ecb0857eb3e0c074a2291b9e
olexandr-mosin <olexandr.mosin@ring.com>
2020-03-25 17:07:52 +0200

    add spring

--- src/main/java/services/PostgreSQLJDBC.java
+++ src/main/java/services/PostgreSQLJDBC.java
@@ -54,28 +54,10 @@ public class PostgreSQLJDBC {
         String sql = String.format("SELECT PRICE FROM SUBSCRIPTIONS WHERE NAME='%s';", name.getStorageName());
         Statement stmt = connection.createStatement();
         ResultSet resultSet = stmt.executeQuery(sql);
-        double price = resultSet.getDouble("name");
+        double price = resultSet.getDouble("PRICE");
         logger.info(sql);
         stmt.close();
         logger.info(price);
         return price;
     }
-
-    /*
-     stmt = c.createStatement();
-         ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
-         while ( rs.next() ) {
-            int id = rs.getInt("id");
-            String  name = rs.getString("name");
-            int age  = rs.getInt("age");
-            String  address = rs.getString("address");
-            float salary = rs.getFloat("salary");
-            System.out.println( "ID = " + id );
-            System.out.println( "NAME = " + name );
-            System.out.println( "AGE = " + age );
-            System.out.println( "ADDRESS = " + address );
-            System.out.println( "SALARY = " + salary );
-            System.out.println();
-         }
-     */
 }
